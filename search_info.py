df6d4863fabfc3734c3064cc22a7560178f63ede
Машенков А.И <55743565+AleksejMaschenkov@users.noreply.github.com>
2022-09-27 12:44:20 +0300

    Update search_info.py

--- search_info.py
+++ search_info.py
@@ -42,9 +42,10 @@ class Filtr(object):
 # ============================================================
 #
 # ============================================================
-
+# ??? regex = re.compile(r"([\(\)])")
 class SISTEMA(object):
     l_firma = ['FAG', 'KOYO', 'NTN', 'SKF', 'NSK', 'ASAHI']
+    l_key = ['подшипн']
 
     def __init__(self, manufacturer=None):
         if manufacturer is not None:
@@ -55,6 +56,7 @@ class SISTEMA(object):
         return p.findall(my_str)
 
     def my_output(self, value):
+        """:return False or List. if len(..)==0 return False """
         if isinstance(value, list) and len(value):
             return value
         return False
@@ -101,8 +103,36 @@ class SISTEMA(object):
         p = re.compile(pattern, re.IGNORECASE)
         return self.my_output(p.findall(my_text))
 
+    def KEY(self, my_text):
+
+        def _perebor(self, temp: list, words: list):
+            aa = []
+            temp = list(set(temp))  # выкидываем повторы
+            for strkey in temp:
+                tl = [word for word in words if strkey in word]
+                aa.append((strkey, tl))
+            return aa
+
+        temp = '|'.join(self.l_key).lower()
+        """ без lower вырубалось на \F r'\FAG|Timken|KOYO|NTN|SKF' """
+
+        """
+        let str = "Следует писать color или colour?";
+        alert( str.match(/colou?r/g) ); // color, colour
+        """
+        pattern = f"\{temp}"
+        p = re.compile(pattern, re.IGNORECASE)
+        ll: list = self.my_output(p.findall(my_text))
+        if ll:
+            itog = _perebor(self, ll, self.word_spliter(my_text))
+        return self.my_output(itog)
+
+    def email(self, text):
+        return re.findall(r'\S+@\S+', text)  # !!!!
+
     def Mix(self, my_text: str):
         """Смесь латинских букв и цифр"""
+
         def cyrillic_has(words):
             # есть русские буквы
             return bool(re.search('[а-яёА-ЯЁ]', words))
@@ -139,6 +169,41 @@ class SISTEMA(object):
         return re.split(r'\W+', txt)
 
 
+# ==================================================================
+
+def Podval(text: str):
+    """Разбиваем text по ключу 'с уважением'
+    txt = 'Часть 1 С уважением Иванов С уважением xxxx'
+    :return ['Часть 1',['С уважением Иванов','С уважением xxxx']]
+    """
+    text_lower: str = text.lower()
+    if 'уважен' in text_lower:
+        zz = text_lower.split('с уважением', maxsplit=-1)
+        # !!! а если встретится 'с     уважением' -> не сработает
+        # zz = text_lower.partition('с уважением') #говорят быстрее
+        if len(zz) == 1:
+            return text, []
+        elif len(zz) == 2:
+            pos = len(zz[0])
+            return text[:pos], [text[pos:]]
+        else:
+            pos = len(zz[0])
+            top_text, bottom_text = text[:pos], text[pos:]
+            # p = re.compile('(с уважением)',re.I)
+            regex = re.compile('(с уважением)', re.I)
+
+            def split_with_separators(regex, s):
+                return list(filter(None, regex.split(s)))
+
+            matches = split_with_separators(regex, bottom_text)
+            # txt = 'С уважением Иванов С уважением xxxx'
+            # matches ['С уважением',' Иванов','С уважением', 'xxxx']
+            # создадим список ['С уважением Иванов','С уважением xxxx']
+            s = matches.copy()
+            t = [s[i] + s[i + 1] for i in range(0, len(s), 2)]
+            return top_text, t
+
+
 # ============================================================
 #                   MAIN (EXAMPLE AND TEST)
 # ============================================================
@@ -146,14 +211,17 @@ class SISTEMA(object):
 
 if __name__ == '__main__':
 
-    mail_text = """ 
+    mail_text = """
         Просим сообщить наличий подшипников SKF 6305-2RS1/C3 (20шт).
         Возможена замена на аналог Timken.
         Можете ли вы поставить их в г.Москву 1700010.
-        С уважением Иванов Иван Иваныч инн 1770000009 АО"Сокол", 
+        Сообщите можете ли поставить подшипники маломагнитные 
+           С уважением Иванов Иван Иваныч инн 1770000009 АО"Сокол", 
         170009 г. Тверь, ул. Победы 10
+        с уважением ivanov@mail.ru
     """
-
+    print(mail_text)
+    print('============= начинаем анализ =====================')
     # ========================================================
     #       create a list of manufacturers [append]
     my_firma = SISTEMA.l_firma.extend(['Timken', 'Craft', 'MPZ'])
@@ -161,26 +229,45 @@ if __name__ == '__main__':
     ReSHATEL = SISTEMA(my_firma)
     # ========================================================
     query = {
-        'mix': ReSHATEL.Mix,
+        'mix(?kod)': ReSHATEL.Mix,
         'firma': ReSHATEL.Firma,
         'inn': ReSHATEL.INN,
         'sity': ReSHATEL.SitySearch,
         'sity1': ReSHATEL.SitySearch1,
+        'for analiz email': ReSHATEL.email,
         'postkodeRU': ReSHATEL.PostKodeRU,
+        'special_word': ReSHATEL.KEY,
+        'Podval': Podval,
     }
 
     my_filtr = Filtr(query)
-    rezult:dict = my_filtr.run(mail_text)
+    rezult: dict = my_filtr.run(mail_text)
     # ============= output  ===================================
     for key, value in rezult.items():
         print(f'{key:30} {value}')
 
+    _ = input('Нажми ENTER для продолжения (в окне вывода)')
+    print('========== Разбор на инфо блоки ===================')
+    razdel = rezult.get('Podval')
+    top = razdel[0]
+    bottom = razdel[1]
+    print(top)
+
+    for txt in bottom:
+        razd = 10 * "-"
+        print(f'{razd:^15}')
+        print(txt)
+    print('------------------------------')
+
     # =======================================================
     """
-    mix                            ['6305-2RS1/C3.']
+    mix(?kod)                      ['6305-2RS1/C3']
     firma                          ['SKF', 'Timken']
     inn                            ['1770000009']
     sity                           ['г. Тверь,']
     sity1                          ['г.Москву 1700010.', 'г. Тверь, ул. Победы 10']
+    for analiz email               ['ivanov@mail.ru']
     postkodeRU                     ['170009']
+    special_word                   [('подшипн', ['подшипников', 'подшипники'])]
+    ......
     """
