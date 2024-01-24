9ff5028a0863470aa7818951bc8e4e084bbc5ca0
Машенков А.И <55743565+AleksejMaschenkov@users.noreply.github.com>
2024-01-24 21:10:32 +0300

    Update Rewrite_mouse_event.py

--- my_PyQt5/Rewrite_mouse_event.py
+++ my_PyQt5/Rewrite_mouse_event.py
@@ -9,13 +9,13 @@ class EXAMPLE(QTextBrowser):
   ...
   def mousePressEvent(self, event):
     if event.button() in [Qt.RightButton, Qt.LeftButton]:
-    print(" @00000 rewrite   Qt.RightButton ")
-            # Rewrite the mouse event to a left button event so the cursor is
-            # moved to the location of the pointer.
-    event = QMouseEvent(QEvent.MouseButtonPress,
-                                event.pos(),
-                                Qt.LeftButton,
-                                Qt.LeftButton,
-                                Qt.NoModifier)
-    get_app().postEvent(self.parent(), event)
+        print(" @00000 rewrite   Qt.RightButton ")
+                # Rewrite the mouse event to a left button event so the cursor is
+                # moved to the location of the pointer.
+        event = QMouseEvent(QEvent.MouseButtonPress,
+                                    event.pos(),
+                                    Qt.LeftButton,
+                                    Qt.LeftButton,
+                                    Qt.NoModifier)
+        get_app().postEvent(self.parent(), event)
          
